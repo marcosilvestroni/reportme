ef7e89a870d38d2f1f59afad48f587de6fabf403
Marco Silvestroni <silvestronimarco@gmail.com>
2020-01-21 00:26:58 +0100

    remove debug knex

--- public/server/index.js
+++ public/server/index.js
@@ -62,7 +62,7 @@ ping.promise.probe(name, { timeout: 2 }).then(res => {
 
 
   const knexConfig = {
-    debug: true,
+    //debug: isDev,
     client: "mssql",
     connection: {
       user: "sa",
